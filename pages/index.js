20a04c3fa6802033dab6b45649f26ea0ac6e53b0
Dennis Passway <dennispassway@gmail.com>
2019-02-04 15:30:59 +0100

    Fix title

--- pages/index.js
+++ pages/index.js
@@ -151,13 +151,13 @@ export default class Index extends Component {
 
           <meta property='og:type' content='website' />
           <meta property='og:url' content={url} />
-          <meta property='og:title' content='{title}' />
+          <meta property='og:title' content={title} />
           <meta property='og:description' content={description} />
           <meta property='og:image' content={ogImage} />
 
           <meta property='twitter:card' content='summary_large_image' />
           <meta property='twitter:url' content={url} />
-          <meta property='twitter:title' content='{title}' />
+          <meta property='twitter:title' content={title} />
           <meta property='twitter:description' content={description} />
           <meta property='twitter:image' content={ogImage} />
 
